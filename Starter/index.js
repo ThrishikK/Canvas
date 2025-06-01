3bd723fb639d20cc4ede60cebd29c698f803bcf8
ThrishikK <kosuruthrishik@gmail.com>
2025-06-01 17:32:12 +0530

    Canvas Starter Created

---
+++ Starter/index.js
@@ -0,0 +1,3 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d"); // Get the 2D drawing context
+console.log(ctx); // This will output the rendering context object
