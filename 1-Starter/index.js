e94898e4b858b14ff62755cf54064b62af74c88a
ThrishikK <kosuruthrishik@gmail.com>
2025-06-02 09:16:16 +0530

    Deleted All Folders

--- 1-Starter/index.js
+++
@@ -1,6 +0,0 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d"); // Get the 2D drawing context
-console.log(ctx); // This will output the rendering context object
-
-canvas.height = 500;
-canvas.width = 500;
