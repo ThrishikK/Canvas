3e60bcef7f0c6d915a844b5bc03c0759efc21a81
ThrishikK <kosuruthrishik@gmail.com>
2025-06-02 06:43:13 +0530

    Sign Changed to compensate canvas inverse Y-axis -> const y = centerY - radius * Math.sin(angleInRadians);

--- 7-Trigonometry/index.js
+++ 7-Trigonometry/index.js
@@ -17,7 +17,7 @@ angles.forEach((each) => {
   const angleInRadians = (each * Math.PI) / 180;
 
   const x = centerX + radius * Math.cos(angleInRadians);
-  const y = centerY + radius * Math.sin(angleInRadians);
+  const y = centerY - radius * Math.sin(angleInRadians);
 
   ctx.beginPath();
   ctx.arc(x, y, 5, 0, Math.PI * 2);
